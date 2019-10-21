6cb76959a53f87ed2ac1940f1512d408062127d1
Wojciech Klodkowski <wklodkowski@pgs-soft.com>
2019-10-21 13:19:16 +0200

    Added visual studio solution for projects

---
+++ src/ApiGateways/Cinemaxum.ApiGateway.Bll/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cinemaxum.ApiGateway.Bll
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/Movie/Cinemaxum.Movie.Dal/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cinemaxum.Movie.Dal
+{
+    public class Class1
+    {
+    }
+}
